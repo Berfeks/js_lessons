e2630b18d75439c2de3276ab32b8e0daaf7442ef
Maksym Plevnytskyi <76871115+Berfeks@users.noreply.github.com>
2021-08-16 21:56:40 +0200

    Update common.js

--- common.js
+++ common.js
@@ -1,9 +1,9 @@
-// inputMask
+
 let inputs = document.querySelectorAll('input[type="tel"]');
 let im = new Inputmask('+7 (999) 999-99-99');
 im.mask(inputs);
 
-// validate
+
 
 function validateForms(selector, rules) {
     new window.JustValidate(selector, {
